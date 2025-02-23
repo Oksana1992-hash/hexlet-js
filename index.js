0edadad2a3103739d6cef3444f87a7cf2ac8e4da
Oksana1992-hash <nox23061992@gmail.com>
2025-02-23 21:06:19 +0300

    add import function from src/index.js

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-console.log("Hello, Hexlet!");
-import _ from "lodash";
-console.log(_.last(["one", "two"]));
-const fn = (a, b) => {
-  const c = a + b;
-  return c;
-};
+import initializeApp from './src/index.js';
+initializeApp();
